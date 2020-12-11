827332baa3f10bfe7a05356bc62d9ace66aba431
Hamed Sedighi <hamedsedighi@gmail.com>
2020-12-11 14:39:07 +0100

    Responsive: Add Lorem txt to interview :wrench:

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import Navbar from '@/components/Navbar'
 import Carousel from '@/components/Carousel'
 import Musicians from '@/components/Musicians'
-import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
