b1bc9a67729b9679d52e5201c48cc471510009b1
Hamed Sedighi <hamedsedighi@gmail.com>
2020-12-23 21:27:15 +0100

    Fix: twitter card meta data :wrench:

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -7,20 +7,20 @@ export default function CustomHead({ homepageTitle, title, coverImage }) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
-        <meta name="description" content="Female Rockers mission is to spread the word for talented musicians and give them a stance where they can professionally present their portfolio." />
+        <meta name="description" content="Our mission is to spread the word for talented musicians and give them a stance where they can professionally present their portfolio." />
         <meta name="keywords" content="interview rocknroll music female rockers rock jazz blues" />
         <meta name="theme-color" content="#dc3545" />
 
         <meta property='og:title' content={title ? `Female Rockers | Exclusive Interview With ${title}`: homepageTitle} />
         <meta property='og:image' content={coverImage || `/screenshot-01.png`} />
-        <meta property='og:description' content='Female Rockers mission is to spread the word for talented musicians' />
+        <meta property='og:description' content='Our mission is to spread the word for talented musicians' />
         <meta property='og:url' content='https://femalerockers.com' />
 
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="femalerockers.com" />
         <meta name="twitter:title" content={title ? `Female Rockers | Exclusive Interview With ${title}`: homepageTitle} />
-        <meta name="twitter:description" content="Female Rockers mission is to spread the word for talented musicians and give them a stance where they can professionally present their portfolio." />
-        {/* <meta name="twitter:image" content={coverImage || `/screenshot-01.png`} /> */}
-        <meta property="twitter:image" content={coverImage || `/screenshot-01.png`}></meta>
-        <meta name="twitter:card" content={title ? `Female Rockers | Exclusive Interview With ${title}`: homepageTitle} />
+        <meta name="twitter:description" content="Our mission is to spread the word for talented musicians and give them a stance where they can professionally present their portfolio." />
+        <meta name="twitter:image" content={coverImage || `/screenshot-01.png`}/>
 
         <link rel="manifest" href="/manifest.json" />
         <link href="/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
