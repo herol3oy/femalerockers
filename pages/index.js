4ba04a9f89dc371b9a2428eecf54fac6caea2a50
Hamed Sedighi <hamedsedighi@gmail.com>
2021-01-13 14:48:32 +0100

    Add scroll to top on homepage :wrench: :rocket:

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import { useEffect } from 'react'
 import Carousel from '@components/Carousel'
 import Musicians from '@components/Musicians'
 import RandomQuote from '@components/RandomQuote'
 import CustomHead from '@components/CustomHead'
 
 export default function Home() {
+  
+  useEffect(() => window.scrollTo({ top: 0 }), [])
+
   return (
     <>
       <CustomHead />
