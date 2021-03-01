afd194fc06eebe58472f3f5fda67c392bcfbd829
Hamed Sedighi <hamedsedighi@gmail.com>
2021-03-01 12:40:25 +0100

    :wrench: Add Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
 	render() {
@@ -10,6 +11,24 @@ export default class MyDocument extends Document {
 						async
 						src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
 					></script>
+
+					{/* Global Site Tag (gtag.js) - Google Analytics */}
+					<script
+						async
+						src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+					/>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+						}}
+					/>
 				</Head>
 				<body>
 					<Main />
