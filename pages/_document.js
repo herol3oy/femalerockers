e182699d7baf5058fd4d8a0525f6bf9bd2293f15
Hamed Sedighi <hamedsedighi@gmail.com>
2021-03-01 12:18:16 +0100

    :package: Add google ads

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang='en'>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+	render() {
+		return (
+			<Html lang='en'>
+				<Head>
+					<script
+						data-ad-client='ca-pub-4305738680942112'
+						async
+						src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+					></script>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
 }
