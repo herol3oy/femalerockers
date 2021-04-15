eb043fde762683c324c8bb4d85bbf3da2c695f36
Hamed Sedighi <hamedsedighi@gmail.com>
2021-04-15 19:27:48 +0200

    :package: Remove lower case shred collab route

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import * as gtag from "../lib/gtag";
-import Link from "next/link";
 import Navbar from "@components/Navbar";
 import Footer from "@components/Footer";
 import Banner from "@components/Banner";
-import CookieConsent from "react-cookie-consent";
+import CookiesConsent from "@components/CookiesConsent";
 import "@styles/main.scss";
 import dynamic from "next/dynamic";
 
@@ -14,7 +13,7 @@ const NewsLetterPopup = dynamic(() => import("@components/NewsLetterPopup"));
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const isBioPage = router.route === "/bio";
-  const isShredCollabPage = router.route === "/ShredCollab" || "/shredcollab";
+  const isShredCollabPage = router.route === "/ShredCollab";
 
   useEffect(() => {
     const handleRouteChange = (url) => {
@@ -31,56 +30,11 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       {!isBioPage && !isShredCollabPage && <Banner />}
-      {!isShredCollabPage && <Navbar />}
+      {!isBioPage && !isShredCollabPage && <Navbar />}
       <Component {...pageProps} />
       {!isBioPage && !isShredCollabPage && <Footer />}
       {!isBioPage && !isShredCollabPage && <NewsLetterPopup />}
-      {!isBioPage && !isShredCollabPage && (
-        <CookieConsent
-          location="bottom"
-          buttonText="Sounds good 😉"
-          style={{
-            background: "#2f2f2f",
-            color: "#fff",
-            justifyContent: "center",
-            margin: "auto",
-            lineHeight: "initial !important",
-          }}
-          buttonStyle={{
-            color: "#fff",
-            backgroundColor: "#dc3545",
-            fontSize: "13px",
-            marginTop: "15px",
-            padding: ".6rem",
-            height: "40px",
-            borderRadius: ".25rem",
-            fontWeight: "bolder  ",
-          }}
-          contentStyle={{
-            flex: "none",
-            width: "fit-content",
-            textAlign: "center",
-          }}
-          expires={150}
-        >
-          <small className="fw-light">
-            We use cookies to give you the best experience possible. Continue
-            browsing or review our {` `}
-            <Link href="/privacypolicy">
-              <a>
-                <u className="text-light">privacy policy</u>
-              </a>
-            </Link>{" "}
-            and{" "}
-            <Link className="text-light" href="/terms">
-              <a>
-                <u className="text-light">terms</u>
-              </a>
-            </Link>
-            {` `} to learn more.
-          </small>
-        </CookieConsent>
-      )}
+      <CookiesConsent />
     </>
   );
 }
--- pages/shredcollab.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function shredcollab() {
-  useEffect(() => {
-    window.location = "https://youtu.be/5mf1Hcl3wnQ";
-  }, []);
-
-  return null;
-}
