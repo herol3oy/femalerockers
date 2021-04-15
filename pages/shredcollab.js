3e5afaa401f86f9c7500b5922d7be6a546a42e7f
Hamed Sedighi <hamedsedighi@gmail.com>
2021-04-15 18:06:14 +0200

    :package: Add lower case shredcollab

---
+++ pages/shredcollab.js
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+export default function shredcollab() {
+  useEffect(() => {
+    window.location = "https://youtu.be/5mf1Hcl3wnQ";
+  }, []);
+
+  return null;
+}
