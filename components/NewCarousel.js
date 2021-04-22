f5981bc15151a24c299df590e05b912d6578c6e9
Hamed Sedighi <hamedsedighi@gmail.com>
2021-04-22 20:12:32 +0200

    :tada: Update slider style on hover

--- components/NewCarousel.js
+++ components/NewCarousel.js
@@ -38,6 +38,10 @@ const responsive = {
 export default function NewCarousel() {
   const [carousel, setCarousel] = useState(null);
   const [sliderBgImg, setSliderBgImg] = useState(null);
+  const [
+    selectedMusicianOnMouseEnter,
+    setSelectedMusicianOnMouseEnter,
+  ] = useState(null);
 
   useEffect(() => {
     sanityClient
@@ -56,6 +60,40 @@ export default function NewCarousel() {
       .catch(console.error);
   }, []);
 
+  const mouseEnterHandle = (url, stageName) => {
+    setSliderBgImg(urlFor(url).url());
+    setSelectedMusicianOnMouseEnter(stageName);
+  };
+
+  const mouseLeaveHandle = () => {
+    setSliderBgImg("");
+    setSelectedMusicianOnMouseEnter(null);
+  };
+
+  const CardBodyText = (rocker) => (
+    <Link href={rocker.slug.current}>
+      <a>
+        <Card.Body className="card-body__description">
+          <div className="position-absolute col-8 text-start align-self-end card-body__wrapper">
+            <h5 className="text-start text-danger fw-bolder m-0">{`${rocker.stageName} ${rocker.country}`}</h5>
+            <h3 className="text-start text-light  card-title">
+              {rocker.title}
+            </h3>
+          </div>
+        </Card.Body>
+      </a>
+    </Link>
+  );
+
+  const renderCardSlider = (rocker) => {
+    if (selectedMusicianOnMouseEnter === null)
+      return <CardBodyText {...rocker} />;
+
+    if (selectedMusicianOnMouseEnter === rocker.stageName) {
+      return <CardBodyText {...rocker} />;
+    }
+  };
+
   return (
     <Container className="my-3">
       <section className="section-wrapper">
@@ -67,16 +105,16 @@ export default function NewCarousel() {
           partialVisbile
           autoPlay
           responsive={responsive}
-          removeArrowOnDeviceType={["tablet", "mobile"]}
+          removeArrowOnDeviceType={["mobile"]}
         >
           {_.map(carousel, (rocker) => (
             <div
               key={rocker.stageName.toString()}
               className="slider-card__wrapper"
               onMouseEnter={() =>
-                setSliderBgImg(urlFor(rocker.coverImage.asset).url())
+                mouseEnterHandle(rocker.coverImage.asset, rocker.stageName)
               }
-              onMouseLeave={() => setSliderBgImg(null)}
+              onMouseLeave={() => mouseLeaveHandle()}
             >
               {!sliderBgImg && (
                 <Image
@@ -87,69 +125,58 @@ export default function NewCarousel() {
                   objectFit="cover"
                 />
               )}
-              <Link href={rocker.slug.current}>
-                <a>
-                  <Card.Body className="card-body__description">
-                    <div className="position-absolute col-8 text-start align-self-end card-body__wrapper">
-                      <h5 className="text-start text-danger fw-bolder m-0">{`${rocker.stageName} ${rocker.country}`}</h5>
-                      <h3 className="text-start text-light  d-lg-block d-none card-title">
-                        {rocker.title}
-                      </h3>
-                      <u>
-                        <strong></strong>
-                      </u>
-                    </div>
-                  </Card.Body>
-                </a>
-              </Link>
-              <style global jsx>{`
-                .section-wrapper {
-                  background-image: url(${sliderBgImg});
-                  background-size: contain;
-                  background-position: center;
-                  // background-repeat: no-repeat;
-                  // height: 30rem;
-                  transition: all 0.8s ease-out;
-                }
-                .profile-img {
-                  border: solid 1px #333 !important;
-                  transition: all 1s ease-in;
-                }
-                .slider-card__wrapper {
-                  border: solid 1px #333 !important;
-                  height: 55vh;
-                  cursor: pointer;
-                }
-                .slider-card__wrapper:hover .card-body__wrapper {
-                  bottom: 50px;
-                  transition: all 0.2s ease-in;
-                }
-                .card-body__wrapper {
-                  z-index: 1;
-                  bottom: 0;
-                  transition: all 0.1s ease-out;
-                }
 
-                .card-body__description::after {
-                  display: flex;
-                  content: "";
-                  position: absolute;
-                  left: 0;
-                  top: 0;
-                  width: 100%;
-                  height: 100%;
-                  display: inline-block;
-                  background: linear-gradient(
-                    to top,
-                    #111 0%,
-                    rgba(255, 255, 255, 0) 100%
-                  );
-                }
-              `}</style>
+              {renderCardSlider(rocker)}
             </div>
           ))}
         </Carousel>
       </section>
+      <style global jsx>{`
+        .section-wrapper {
+          background-image: url(${sliderBgImg});
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;
+          transition: all 0.8s ease-out;
+        }
+
+        .profile-img {
+          border: solid 1px #333 !important;
+        }
+
+        .slider-card__wrapper {
+          border: solid 1px #333 !important;
+          height: 55vh;
+          cursor: pointer;
+        }
+
+        .card-body__wrapper {
+          z-index: 1;
+          bottom: 0;
+          transition: all 0.7s ease-in;
+        }
+
+        .slider-card__wrapper:hover .card-body__wrapper {
+          bottom: 50px;
+          transition: all 0.8s ease-in;
+        }
+
+        .card-body__description::after {
+          display: flex;
+          content: "";
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
+          display: inline-block;
+          background: linear-gradient(
+            to top,
+            #111 0%,
+            rgba(255, 255, 255, 0) 100%
+          );
+        }
+      `}</style>
     </Container>
   );
 }
