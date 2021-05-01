86af1cff9ddd8e623ddffb54e97f36b54b2da899
Hamed Sedighi <hamedsedighi@gmail.com>
2021-05-01 23:33:34 +0200

    :nail_cate: Add logo with transition on hover

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,36 +1,47 @@
 import Link from "next/link";
 import Navbar from "@BS/Navbar";
 import Container from "@BS/Container";
-
+import LogoSvg from "@components/LogoSvg";
 export default function navbar() {
   return (
     <Container fluid>
       <Navbar
         className="
         container
-        d-flex 
+        d-flex
+        flex-column-reverse
+        flex-lg-row
+        justify-content-md-between
         justify-content-center 
+        align-items-center
         my-3"
         expand="lg"
       >
-        <section className="text-center">
+        <div className="text-center text-lg-start text-md-center text-md-start mt-2 mt-lg-0">
           <Link href="/">
             <a
               className="
-              site__title 
-              h5 
-              text-danger 
-              fw-bolder 
-              text-decoration-none 
-              text-uppercase"
+                site__title
+                h5
+                text-danger
+                fw-bolder
+                text-decoration-none
+                text-uppercase
+                mt-3
+                "
             >
               Female Rockers
             </a>
           </Link>
-          <p className="h6 fw-lighter text-light">
+          <p className="small fw-lighter text-light">
             interviews future sensations in rock music
           </p>
-        </section>
+        </div>
+        <Link href="/">
+          <a className="logo__svg">
+            <LogoSvg />
+          </a>
+        </Link>
       </Navbar>
     </Container>
   );
