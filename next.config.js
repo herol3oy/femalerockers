1fdda8009a602718cebd9004b9e3aabe02dd9928
Hamed Sedighi <hamedsedighi@gmail.com>
2021-11-23 23:07:30 +0100

    Add preact :tada:

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
+const withPreact = require('next-plugin-preact')
 
-module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === "development",
-    dest: "public",
-    runtimeCaching,
-  },
-  images: {
-    domains: ["cdn.sanity.io", "res.cloudinary.com"],
-  },
-  swcMinify: true,
-});
+module.exports = withPreact(
+  withPWA({
+    pwa: {
+      disable: process.env.NODE_ENV === "development",
+      dest: "public",
+      runtimeCaching,
+    },
+    images: {
+      domains: ["cdn.sanity.io", "res.cloudinary.com"],
+    },
+    swcMinify: true,
+  })
+);
