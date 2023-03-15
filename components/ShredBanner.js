a3314788c1f719de78d18c6d44325554766356be
Hamed <hamedsedighi@gmail.com>
2023-03-15 16:50:34 +0100

    update youtube section layout

--- components/ShredBanner.js
+++ components/ShredBanner.js
@@ -1,91 +1,85 @@
 import { useState } from "react";
-import Image from "next/legacy/image";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import { FaYoutube } from "/node_modules/react-icons/fa";
+
+const SHRED_BANNERS = [
+  {
+    href: "https://youtu.be/5mf1Hcl3wnQ",
+    imgSrc:
+      "https://res.cloudinary.com/dxu6gcib2/image/upload/v1619175748/Female%20Rockers/female-rockers-shred-collab-united-youtube-cover_ke2yuo.jpg",
+    alt: "Female Rockers United",
+  },
+  {
+    href: "https://youtu.be/04cnfAICCM4",
+    imgSrc:
+      "https://res.cloudinary.com/dxu6gcib2/image/upload/v1619526452/Female%20Rockers/female-rockers-cover-muse-youtube-banner_ynggqc.jpg",
+    alt: "Muse Cover",
+  },
+];
 
 export default function ShredBanner() {
-  const [bgColor, bgColorSet] = useState("#111111");
+  const [hoveredBanner, setHoveredImage] = useState(null);
 
-  const mouseOverHandle = () => {
-    bgColorSet("black");
-  };
+  const handleMouseEnter = (bannerIndex) => setHoveredImage(bannerIndex);
+  const handleMouseLeave = () => setHoveredImage(null);
 
   return (
     <Container>
-      <Row className="my-5 g-0">
-        <section className="col-12 col-lg-6">
-          <a
-            href="https://youtu.be/5mf1Hcl3wnQ"
-            className="shred-banner "
-            target="_blank"
-            onMouseEnter={mouseOverHandle}
-          >
-            <Image
-              className="cover__img"
-              alt="Female Rockers Shred Collab"
-              src="https://res.cloudinary.com/dxu6gcib2/image/upload/v1619175748/Female%20Rockers/female-rockers-shred-collab-united-youtube-cover_ke2yuo.jpg"
-              layout="responsive"
-              width={900}
-              height={250}
-              objectFit="cover"
-            />
-          </a>
-        </section>
-
-        <section className="mt-3 mt-lg-0 col-12 col-lg-6">
-          <a
-            href="https://youtu.be/04cnfAICCM4"
-            className="shred-banner position-relative"
-            target="_blank"
-            onMouseEnter={mouseOverHandle}
-          >
-            <Image
-              className="cover__img"
-              alt="Female Rockers - guitar & drum Cover - Muse"
-              src="https://res.cloudinary.com/dxu6gcib2/image/upload/v1619526452/Female%20Rockers/female-rockers-cover-muse-youtube-banner_ynggqc.jpg"
-              layout="responsive"
-              width={900}
-              height={250}
-              objectFit="cover"
-            />
-          </a>
-        </section>
-        <div className="bg"></div>
+      <Row className="my-5">
+        <div className="col-12 col-lg-2 mt-2">
+          <div className="d-none flex-column text-center d-sm-flex align-items-center justify-content-center bg-danger rounded-2 py-5 text-light">
+            <FaYoutube className="display-1" />
+          </div>
+        </div>
+        {SHRED_BANNERS.map((banner, bannerIndex) => {
+          const isHovered = bannerIndex === hoveredBanner;
+          return (
+            <div className="col-12 col-lg-5 mt-2">
+              <section
+                className="banner-overlay overflow-hidden rounded-2 p-5"
+                style={{
+                  background: `url(${banner.imgSrc}) no-repeat`,
+                  backgroundPosition: "center",
+                  backgroundSize: isHovered ? "95%" : "160%",
+                }}
+                key={banner.href}
+              >
+                <div>
+                  <h2 className="fw-bold text-light">{banner.alt}</h2>
+                  <a href={banner.href} target="_blank">
+                    <Button
+                      className="btn btn-danger btn-lg  "
+                      onMouseEnter={() => handleMouseEnter(bannerIndex)}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      Watch
+                    </Button>
+                  </a>
+                </div>
+              </section>
+            </div>
+          );
+        })}
       </Row>
       <style global jsx>{`
-        .shred-banner {
-          transition: transform 0.2s ease-in-out;
-          transform: scale(1);
+        .banner-overlay {
+          position: relative;
+          background-size: cover;
+          z-index: 1;
+          transition: all 300ms ease-in-out;
         }
 
-        .shred-banner:hover {
-          transition: transform 0.2s ease-in-out;
-          transform: scale(0.95);
-        }
-
-        .shred-banner img {
-          transition: transform 0.1s ease-in-out;
-          transform: rotate(0deg) scale(1);
-        }
-
-        .shred-banner:hover img {
-          transition: transform 0.3s ease-in-out;
-          transform: rotate(0deg) scale(0.95);
-        }
-
-        section:hover ~ .bg {
-          background-color: ${bgColor};
-        }
-
-        .bg {
-          position: fixed;
-          z-index: -2;
+        .banner-overlay::after {
+          position: absolute;
+          content: "";
           top: 0;
+          left: 0;
           right: 0;
           bottom: 0;
-          left: 0;
-          transition: 0.25s;
-          pointer-events: none;
+          z-index: -1;
+          background: rgba(0, 0, 0, 0.8);
         }
       `}</style>
     </Container>
