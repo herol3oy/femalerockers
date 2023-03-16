caa0787ca2ad6561eba06a4ae1409c5b682248bd
Hamed <hamedsedighi@gmail.com>
2023-03-16 18:53:53 +0100

    update get featured page

--- pages/GetFeaturedNow.js
+++ pages/GetFeaturedNow.js
@@ -1,11 +1,64 @@
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Logo from "@components/svg/Logo";
 import Link from "next/link";
 import Toast from "react-bootstrap/Toast";
 import Badge from "react-bootstrap/Badge";
 import GetFeaturedLogo from "../components/svg/GetFeaturedLogo";
+
+const TOASTS = [
+  {
+    badge: "1",
+    icon: "🗳️",
+    body: (
+      <h5 className="lh-base">
+        Vote for Gray Dream on this page:{" "}
+        <a
+          target="_blank"
+          href="https://www.fib-risingstars.com/artists/2025"
+          className="link-danger"
+        >
+          FIB FEST 2023
+        </a>{" "}
+      </h5>
+    ),
+  },
+  {
+    badge: "2",
+    icon: "📩",
+    body: (
+      <h5 className="lh-base">
+        After confirming your email and vote, send the final page's screenshot
+        to{" "}
+        <a
+          target="_blank"
+          href="https://www.instagram.com/morriemusician/"
+          className="link-danger"
+        >
+          @MorrieMusician
+        </a>{" "}
+        (the founder of Female Rockers)
+      </h5>
+    ),
+  },
+  {
+    badge: "3",
+    icon: "🥳",
+    body: (
+      <h5 className="lh-base">
+        Add{" "}
+        <a
+          target="_blank"
+          href="https://www.instagram.com/p/Cpr5XCkssW9/"
+          className="link-danger"
+        >
+          this post
+        </a>{" "}
+        to your story
+      </h5>
+    ),
+  },
+];
 
 export default function GetFeatured() {
   return (
@@ -13,7 +66,6 @@ export default function GetFeatured() {
       <Row className="text-center d-flex justify-content-center mt-5">
         <Col xs={12}>
           <Link href="/" className="text-decoration-none">
-            {/* <Logo className="logo__svg" /> */}
             <GetFeaturedLogo width="200" />
           </Link>
           <a
@@ -28,77 +80,31 @@ export default function GetFeatured() {
             </p>
           </a>
           <hr />
-          <h4 className="text-light fw-bold w-75  m-auto">
+          <h5 className="text-light lh-base fw-bold w-75  m-auto">
             Get guaranteed feature on Female Rockers as an Instagram Story
-          </h4>
+          </h5>
         </Col>
         <p className="text-light my-4 w-50">
           In order to get featured – guaranteed – in our Instagram story, here's
           what you need to do:
         </p>
-        <Col xs={12}>
-          <Toast className="m-auto">
-            <Toast.Header
-              closeButton={false}
-              className="d-flex justify-content-between"
-            >
-              <Badge pill className="bg-danger">
-                1
-              </Badge>
-              <small>🗳️</small>
-            </Toast.Header>
-            <Toast.Body className="bg-dark text-light">
-              Vote for Gray Dream on this page:{` `}
-              <a
-                target="_blank"
-                href="https://www.fib-risingstars.com/artists/2025"
-                className="link-danger"
+        <Col xs={12} className="mb-5">
+          {TOASTS.map((toast) => (
+            <Toast key={toast.badge} className="m-auto mt-4">
+              <Toast.Header
+                closeButton={false}
+                className="d-flex justify-content-between"
               >
-                FIB FEST 2023
-              </a>
-              {` `}
-            </Toast.Body>
-          </Toast>
-
-          <Toast className="m-auto mt-4">
-            <Toast.Header
-              closeButton={false}
-              className="d-flex justify-content-between"
-            >
-              <Badge pill className="bg-danger">
-                2
-              </Badge>
-              <small>📩</small>
-            </Toast.Header>
-            <Toast.Body className="bg-dark text-light">
-              After confirming your email and vote, send the final page's
-              screenshot to
-              <a
-                target="_blank"
-                href="https://www.instagram.com/morriemusician/"
-                className="link-danger"
-              >
-                @MorrieMusician
-              </a>
-              {` `}
-              (the founder of Female Rockers)
-            </Toast.Body>
-          </Toast>
-
-          <Toast className="m-auto mt-4">
-            <Toast.Header
-              closeButton={false}
-              className="d-flex justify-content-between"
-            >
-              <Badge pill className="bg-danger">
-                3
-              </Badge>
-              <small>🥳</small>
-            </Toast.Header>
-            <Toast.Body className="bg-dark text-light">
-              You'll be guaranteed a story on Female Rocker's Instagram.
-            </Toast.Body>
-          </Toast>
+                <Badge pill className="bg-danger">
+                  {toast.badge}
+                </Badge>
+                <small>{toast.icon}</small>
+              </Toast.Header>
+              <Toast.Body className="bg-dark text-light text-start">
+                {toast.body}
+              </Toast.Body>
+            </Toast>
+          ))}
         </Col>
       </Row>
     </Container>
