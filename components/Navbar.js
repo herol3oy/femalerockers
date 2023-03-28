5f818ae285ce3c617f1c0791a608d897fb375b59
Hamed <hamedsedighi@gmail.com>
2023-03-28 23:23:18 +0200

    add animation to logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import TypeFace from "@components/svg/TypeFace";
+import Logo from "@components/svg/Logo";
+import Image from "next/legacy/image";
 
 export default function navbar() {
   return (
@@ -18,8 +19,16 @@ export default function navbar() {
         my-3"
         expand="lg"
       >
-        <Link href="/" className="m-auto mt-2">
-          <TypeFace style={{ width: "150px" }} />
+        <Link href="/" className="m-auto d-flex gap-2">
+          <Logo className="logo__svg" />
+          <Image
+            className=""
+            src="/female-rockers-text-svg.svg"
+            alt="Female rockers logo"
+            layout="fixed"
+            width={90}
+            height={170}
+          />
         </Link>
       </Navbar>
     </Container>
