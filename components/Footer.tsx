1b13cca2af329daa7b0ac05bf5ffd20c11355fe3
herol3oy <hamedsedighi@gmail.com>
2024-12-16 23:43:33 +0100

    update topboar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,36 @@
-import { FOOTER_SECTIONS } from '@/constants/footer-sections'
+import Link from 'next/link'
+
+const currentYear = new Date().getFullYear()
 
 export default function Footer() {
   return (
-    <footer className="secondary-light-color my-20 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
-      {FOOTER_SECTIONS.map((section) => (
-        <section key={section.title}>
-          <h3 className="w-full border border-l-0 border-r-0 border-t-0 border-b-zinc-100 pb-1 text-xl font-bold uppercase">
-            {section.title}
-          </h3>
-          {section.content}
-        </section>
-      ))}
+    <footer className="secondary-light-color my-20">
+      <section>
+        <h3 className="w-full pb-1 text-xl font-bold uppercase">
+          Female Rockers
+        </h3>
+        <hr className="mb-5 mt-1 border border-l-0 border-r-0 border-t-0 border-b-zinc-100 " />
+        <p className="mb-1 text-sm">
+          Our mission is to spread the word for talented musicians and give them
+          a stance where they can professionally present their portfolio
+        </p>
+        <small className="rounded-sm bg-zinc-800 p-1">
+          <Link
+            className="underline transition hover:text-red-400"
+            href="/page/privacy-policy-for-female-rockers-blog"
+          >
+            Privacy Policy
+          </Link>
+          {` `} and {` `}
+          <Link
+            className="underline transition hover:text-red-400"
+            href="/page/terms-and-conditions"
+          >
+            Terms
+          </Link>{' '}
+          {` `}© 2020 - {currentYear}
+        </small>
+      </section>
     </footer>
   )
 }
--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function TopBar() {
   return (
-    <nav className="my-10 flex w-full justify-center">
+    <nav className="my-10 flex w-full flex-col items-center justify-between gap-5 lg:flex-row">
       <Link className="group flex items-center gap-2" href="/">
         <Image
           className="w-16 cursor-pointer transition-all duration-300 ease-in-out group-hover:-rotate-[360deg] md:w-24"
@@ -22,6 +22,40 @@ export default function TopBar() {
           sizes="100vw"
         />
       </Link>
+
+      <h1 className="w-full text-center font-new-rocker text-3xl font-normal not-italic tracking-wide text-slate-50 shadow-sm lg:w-1/4">
+        <span className="block lg:inline-block">
+          Interviews Future Sensations in Rock Music
+        </span>
+      </h1>
+
+      <ul className="flex gap-8 text-lg text-slate-50">
+        <li>
+          <Link
+            className="text-sm font-bold transition hover:text-red-400 lg:text-lg"
+            href="/page/about"
+          >
+            about
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="text-sm font-bold transition hover:text-red-400 lg:text-lg"
+            href="/page/contact"
+          >
+            contact
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="text-sm font-bold transition hover:text-red-400 lg:text-lg"
+            href="https://instagram.com/female_rockers/"
+            target="_blank"
+          >
+            instagram
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 }
