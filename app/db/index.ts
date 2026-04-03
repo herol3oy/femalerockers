b7e6b676d10a7b560c2dac4813bfd272d444da76
Hamed Sedighi <hamedsedighi@gmail.com>
2026-04-03 21:08:26 +0200

    add dotenv quiet

--- app/db/index.ts
+++ app/db/index.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-config({ path: '.env.local' });
+config({ path: '.env.local', quiet: true });
 
 const client = postgres(process.env.DATABASE_URL!);
 export const db = drizzle({ client });
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 
-config({ path: '.env.local' });
+config({ path: '.env.local', quiet: true });
 
 export default defineConfig({
   schema: './app/db/schema.ts',
