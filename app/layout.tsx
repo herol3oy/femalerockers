4202096782ed60637a33d973d07bb068d98864e5
Hamed Sedighi <hamedsedighi@gmail.com>
2026-04-04 18:30:41 +0200

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Roboto, Space_Grotesk } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const spaceGroteskHeading = Space_Grotesk({subsets:['latin'],variable:'--font-heading'});
+
+const roboto = Roboto({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +20,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className="h-full antialiased"
+      className={cn("h-full antialiased", "font-sans", roboto.variable, spaceGroteskHeading.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
